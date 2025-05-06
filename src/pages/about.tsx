005e8df9f90722ecb50adbb67fcf232f7446e77f
f2firdaus <firdausnaseem@gmail.com>
2025-05-06 14:13:24 +0530

    add: deployed_link

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,6 +5,7 @@ const About = () => {
       <>
           
           <a  href="https://github.com/f2firdaus/Loan_Calculator" target="_blank">Github Repo</a>    
+          <a  href="https://loanemicalculator1.netlify.app/" target="_blank">Live Deployed Link</a>    
          
           
       </>
